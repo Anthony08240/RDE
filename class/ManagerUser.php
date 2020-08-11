65d71299cb4086f24bbf478861ffabcd0c01a284
Anthony08240 <beenen@simplon-charleville.fr>
2020-08-11 15:40:12 +0200

    inscription caralo express en poo ok

--- class/ManagerUser.php
+++ class/ManagerUser.php
@@ -1,5 +1,7 @@
 <?php
 
+// connexion QCM
+
 class ManagerUser {
     //variable de notre classe
     private $_bdd;
@@ -20,9 +22,9 @@ class ManagerUser {
         $resultat = $connexion->fetch();
         $nbresultats = $connexion->rowCount();
 
-        session_start();
-
         if ($nbresultats == 1) {
+
+            session_start();
 
             $_SESSION['team'] = $resultat['team_name'];
 
@@ -31,6 +33,179 @@ class ManagerUser {
             header('location: ../index.php?success=1');
         }
   }
+
+}
+
+
+// inscription QCM
+
+class ManagerUserIsncriptionExpress {
+    //variable de notre classe
+    private $_bdd;
+
+    //création du constructeur
+    public function __construct($bdd) {
+      $this->_bdd = $bdd;
+    }
+
+
+
+    public function inscription_express($insc_express){
+
+        $teamname = $insc_express->getTeamname();
+
+        $objetinscrip = utf8_decode("Confirmation d'inscription au Carolo Express");
+        $messageinscrip = utf8_decode("Bonjour l'équipe '$teamname', votre inscription est bien prise en compte pour le Carolo Express. A très bientôt ! <br><br>Merci de ne pas répondre à ce mail, pour toute demande merci de vous rendre sur la page contact du site prévue à cet effet.");
+
+
+
+        $teamnameexist = $this->_bdd->prepare("SELECT team_name FROM rdeexpressregister WHERE team_name = '$teamname'");
+        $teamnameexist->execute();
+
+        $count = $teamnameexist->rowCount();
+        if($count==0) {
+            if($insc_express->getMail1() == $insc_express->getVerifmail1() && $insc_express->getMail2() == $insc_express->getVerifmail2() && $insc_express->getMail3() == $insc_express->getVerifmail3() && $insc_express->getMail4() == $insc_express->getVerifmail4() && $insc_express->getMail5() == $insc_express->getVerifmail5()) {
+
+                if($insc_express->getPassword() == $insc_express->getVerifpassword()) {
+                    $part1 = $this->_bdd->prepare("INSERT INTO rdeparticipants (name, first_name, phone, mail)
+                                            VALUES ( :name, :first_name, :phone, :mail)");
+
+                    $part1->execute(array(
+                        ':name' => $insc_express->getName1(),
+                        ':first_name' => $insc_express->getFirstname1(),
+                        ':phone' => $insc_express->getTel1(),
+                        ':mail' => $insc_express->getMail1()
+                    ));
+                    $part1->closeCursor();
+                    $idpart1 = $this->_bdd->lastInsertId();
+
+                    $part2 = $this->_bdd->prepare("INSERT INTO rdeparticipants (name, first_name, phone, mail)
+                                            VALUES ( :name, :first_name, :phone, :mail)");
+
+                    $part2->execute(array(
+                        ':name' => $insc_express->getName2(),
+                        ':first_name' => $insc_express->getFirstname2(),
+                        ':phone' => $insc_express->getTel2(),
+                        ':mail' => $insc_express->getMail2()
+                    ));
+                    $part2->closeCursor();
+                    $idpart2 = $this->_bdd->lastInsertId();
+
+                    $part3 = $this->_bdd->prepare("INSERT INTO rdeparticipants (name, first_name, phone, mail)
+                                            VALUES ( :name, :first_name, :phone, :mail)");
+
+                    $part3->execute(array(
+                        ':name' => $insc_express->getName3(),
+                        ':first_name' => $insc_express->getFirstname3(),
+                        ':phone' => $insc_express->getTel3(),
+                        ':mail' => $insc_express->getMail3()
+                    ));
+                    $part3->closeCursor();
+                    $idpart3 = $this->_bdd->lastInsertId();
+
+                    $part4 = $this->_bdd->prepare("INSERT INTO rdeparticipants (name, first_name, phone, mail)
+                                            VALUES ( :name, :first_name, :phone, :mail)");
+
+                    $part4->execute(array(
+                        ':name' => $insc_express->getName4(),
+                        ':first_name' => $insc_express->getFirstname4(),
+                        ':phone' => $insc_express->getTel4(),
+                        ':mail' => $insc_express->getMail4()
+                    ));
+                    $part4->closeCursor();
+                    $idpart4 = $this->_bdd->lastInsertId();
+
+                    $part5 = $this->_bdd->prepare("INSERT INTO rdeparticipants (name, first_name, phone, mail)
+                                            VALUES ( :name, :first_name, :phone, :mail)");
+
+                    $part5->execute(array(
+                        ':name' => $insc_express->getName5(),
+                        ':first_name' => $insc_express->getFirstname5(),
+                        ':phone' => $insc_express->getTel5(),
+                        ':mail' => $insc_express->getMail5()
+                    ));
+                    $part5->closeCursor();
+                    $idpart5 = $this->_bdd->lastInsertId();
+
+                    $basketregistration = $this->_bdd->prepare("INSERT INTO rdeexpressregister (team_name, establishment, password_manif)
+                                                        VALUES ( :team_name, :establishment, :password_manif)");
+
+                    $basketregistration->execute(array(
+                    ':team_name' => $insc_express->getTeamname(),
+                    ':establishment' => $insc_express->getEstablishment(),
+                    ':password_manif' => $insc_express->getPassword()
+                    ));
+                    $basketregistration->closeCursor();
+
+                    $basketrelation1 = $this->_bdd->prepare("INSERT INTO rdeexpressrelation (participant_id, name_team)
+                                                        VALUES ( :participant_id, :name_team)");
+
+                    $basketrelation1->execute(array(
+                    ':participant_id' => $idpart1,
+                    ':name_team' => $insc_express->getTeamname()
+                    ));
+                    $basketrelation1->closeCursor();
+
+                    $basketrelation2 = $this->_bdd->prepare("INSERT INTO rdeexpressrelation (participant_id, name_team)
+                                                        VALUES ( :participant_id, :name_team)");
+
+                    $basketrelation2->execute(array(
+                    ':participant_id' => $idpart2,
+                    ':name_team' => $insc_express->getTeamname()
+                    ));
+                    $basketrelation2->closeCursor();
+
+                    $basketrelation3 = $this->_bdd->prepare("INSERT INTO rdeexpressrelation (participant_id, name_team)
+                                                        VALUES ( :participant_id, :name_team)");
+
+                    $basketrelation3->execute(array(
+                    ':participant_id' => $idpart3,
+                    ':name_team' => $insc_express->getTeamname()
+                    ));
+                    $basketrelation3->closeCursor();
+
+                    $basketrelation4 = $this->_bdd->prepare("INSERT INTO rdeexpressrelation (participant_id, name_team)
+                                                        VALUES ( :participant_id, :name_team)");
+
+                    $basketrelation4->execute(array(
+                    ':participant_id' => $idpart4,
+                    ':name_team' => $insc_express->getTeamname()
+                    ));
+                    $basketrelation4->closeCursor();
+
+                    $basketrelation5 = $this->_bdd->prepare("INSERT INTO rdeexpressrelation (participant_id, name_team)
+                                                        VALUES ( :participant_id, :name_team)");
+
+                    $basketrelation5->execute(array(
+                    ':participant_id' => $idpart5,
+                    ':name_team' => $insc_express->getTeamname()
+                    ));
+                    $basketrelation5->closeCursor();
+
+                    mail($insc_express->getMail1(), $objetinscrip, $messageinscrip);
+                    mail($insc_express->getMail2(), $objetinscrip, $messageinscrip);
+                    mail($insc_express->getMail3(), $objetinscrip, $messageinscrip);
+                    mail($insc_express->getMail4(), $objetinscrip, $messageinscrip);
+                    mail($insc_express->getMail5(), $objetinscrip, $messageinscrip);
+
+                    header('location: ../event_register_caroloexpress.php?success=1');
+
+                    }
+                    else {
+                        header('location: ../event_register_caroloexpress.php?success=2');
+                    }
+                
+                    
+
+                } else {
+                    header('location: ../event_register_caroloexpress.php?success=3');
+            }
+        }
+        else {
+            header('location: ../event_register_caroloexpress.php?success=4');
+        }
+
+    }
 
 }
     
--- class/User.php
+++ class/User.php
@@ -30,4 +30,165 @@ class User {
     }
 
 }
+
+
+class UserExpress {
+
+  private $_establishment;
+  private $_teamname;
+  private $_password;
+  private $_verifpassword;
+  private $_name1;
+  private $_firstname1;
+  private $_tel1;
+  private $_mail1;
+  private $_verifmail1;
+  private $_name2;
+  private $_firstname2;
+  private $_tel2;
+  private $_mail2;
+  private $_verifmail2;
+  private $_name3;
+  private $_firstname3;
+  private $_tel3;
+  private $_mail3;
+  private $_verifmail3;
+  private $_name4;
+  private $_firstname4;
+  private $_tel4;
+  private $_mail4;
+  private $_verifmail4;
+  private $_name5;
+  private $_firstname5;
+  private $_tel5;
+  private $_mail5;
+  private $_verifmail5;
+
+
+  public function __construct($establishment, $teamname, $password, $verifpassword, $name1, $firstname1, $tel1, $mail1, $verifmail1, 
+  $name2, $firstname2, $tel2, $mail2, $verifmail2, $name3, $firstname3, $tel3, $mail3, $verifmail3, 
+  $name4, $firstname4, $tel4, $mail4, $verifmail4, $name5, $firstname5, $tel5, $mail5, $verifmail5){
+
+      $this->_establishment = $establishment;
+      $this->_teamname = $teamname;
+      $this->_password = $password;
+      $this->_verifpassword = $verifpassword;
+      $this->_name1 = $name1;
+      $this->_firstname1 = $firstname1;
+      $this->_tel1 = $tel1;
+      $this->_mail1 = $mail1;
+      $this->_verifmail1 = $verifmail1;
+      $this->_name2 = $name2;
+      $this->_firstname2 = $firstname2;
+      $this->_tel2 = $tel2;
+      $this->_mail2 = $mail2;
+      $this->_verifmail2 = $verifmail2;
+      $this->_name3 = $name3;
+      $this->_firstname3 = $firstname3;
+      $this->_tel3 = $tel3;
+      $this->_mail3 = $mail3;
+      $this->_verifmail3 = $verifmail3;
+      $this->_name4 = $name4;
+      $this->_firstname4 = $firstname4;
+      $this->_tel4 = $tel4;
+      $this->_mail4 = $mail4;
+      $this->_verifmail4 = $verifmail4;
+      $this->_name5 = $name5;
+      $this->_firstname5 = $firstname5;
+      $this->_tel5 = $tel5;
+      $this->_mail5 = $mail5;
+      $this->_verifmail5 = $verifmail5;
+  }
+
+
+  //listing des getters
+  public function getEstablishment(){
+    return $this->_establishment;
+   }
+   public function getTeamname(){
+    return $this->_teamname;
+   }
+   public function getPassword(){
+    return $this->_password;
+   }
+   public function getVerifpassword(){
+    return $this->_verifpassword;
+   }
+   public function getName1(){
+    return $this->_name1;
+   }
+   public function getFirstname1(){
+    return $this->_firstname1;
+   }
+   public function getTel1(){
+    return $this->_tel1;
+   }
+   public function getMail1(){
+    return $this->_mail1;
+   }
+   public function getVerifmail1(){
+    return $this->_verifmail1;
+   }
+   public function getName2(){
+    return $this->_name2;
+   }
+   public function getFirstname2(){
+    return $this->_firstname2;
+   }
+   public function getTel2(){
+    return $this->_tel2;
+   }
+   public function getMail2(){
+    return $this->_mail2;
+   }
+   public function getVerifmail2(){
+    return $this->_verifmail2;
+   }
+   public function getName3(){
+    return $this->_name3;
+   }
+   public function getFirstname3(){
+    return $this->_firstname3;
+   }
+   public function getTel3(){
+    return $this->_tel3;
+   }
+   public function getMail3(){
+    return $this->_mail3;
+   }
+   public function getVerifmail3(){
+    return $this->_verifmail3;
+   }
+   public function getName4(){
+    return $this->_name4;
+   }
+   public function getFirstname4(){
+    return $this->_firstname4;
+   }
+   public function getTel4(){
+    return $this->_tel4;
+   }
+   public function getMail4(){
+    return $this->_mail4;
+   }
+   public function getVerifmail4(){
+    return $this->_verifmail4;
+   }
+   public function getName5(){
+    return $this->_name5;
+   }
+   public function getFirstname5(){
+    return $this->_firstname5;
+   }
+   public function getTel5(){
+    return $this->_tel5;
+   }
+   public function getMail5(){
+    return $this->_mail5;
+   }
+   public function getVerifmail5(){
+    return $this->_verifmail5;
+   }
+
+}
 ?>
